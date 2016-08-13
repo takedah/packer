8bcabe62d9f1f6da2deb1f649fe6f5521711d142
takedah <takedahiroki@gmail.com>
2016-08-13 13:59:34 +0900

    add proxy setting.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Packer
-CentOS 6.7のVagrant用boxファイルを作成するものです。
-chefのインストールまで行います。
+# Packer for Vagrant Box with proxy environment
+
+## Overview
+
+proxy環境下でCentOS 6.8のVagrant用boxファイルを作成します。
+
+chefのインストールまで行うようにしています。
+
+kickstartのファイルを読めるようにするため、Packerを実行するホストにHTTPサーバを建て、公開ルートディレクトリに ks.cfg ファイルをコピーしてからPackerを実行する必要があります。
--- base.sh
+++ base.sh
@@ -1,3 +1,7 @@
+# proxyサーバのIPアドレスとポート番号を設定。
+PROXY_ADDR="10.1.4.2"
+PROXY_PORT=8080
+
 /usr/sbin/setenforce 0
 sudo sed -i "s/#UseDNS yes/UseDNS no/" /etc/ssh/sshd_config
 
@@ -9,11 +13,21 @@ enabled=0
 gpgcheck=0
 EOM
 
-sudo yum -y install gcc make automake autoconf libtool gcc-c++ kernel-headers-`uname -r` kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget nfs-utils bind-utils
+cat <<EOM | sudo tee -a /etc/yum.conf
+proxy=http://${PROXY_ADDR}:${PROXY_PORT}/
+EOM
+
+sudo sed -i -e 's/^#baseurl/baseurl/' /etc/yum.repos.d/CentOS-Base.repo
+sudo sed -i -e 's/mirror\.centos\.org/ftp\.iij\.ad\.jp\/pub\/linux/' /etc/yum.repos.d/CentOS-Base.repo
+sudo sed -i -e 's/^mirrorlist/#mirrorlist/' /etc/yum.repos.d/CentOS-Base.repo
+sudo rpm --httpproxy $PROXY_ADDR --httpport $PROXY_PORT --import http://ftp.iij.ad.jp/pub/linux/centos/RPM-GPG-KEY-CentOS-6
+
+sudo yum -y install gcc make automake autoconf libtool gcc-c++ kernel-headers-`uname -r` kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget nfs-utils bind-utils policycoreutils-python
 sudo yum -y --enablerepo=epel install dkms
+sudo yum clean all
 
 mkdir -pm 700 /home/vagrant/.ssh
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
+curl -x $PROXY_ADDR:$PROXY_PORT "https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub" -O /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
@@ -23,5 +37,4 @@ sudo sh /mnt/VBoxLinuxAdditions.run
 sudo umount /mnt
 
 sudo /etc/rc.d/init.d/vboxadd setup
-curl -L https://www.chef.io/chef/install.sh | sudo bash
-
+curl -x $PROXY_ADDR:$PROXY_PORT -L https://www.chef.io/chef/install.sh | sudo bash
